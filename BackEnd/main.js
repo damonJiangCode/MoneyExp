44f148dd72b636352df360fda443317ebdb4bc0b
Damon Jiang <damonjiang0@gmail.com>
2023-11-07 01:18:29 -0600

    BackEnd and FrontEnd test  completed

---
+++ BackEnd/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+const { dir } = require('node:console')
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+  
+    win.loadFile(path.join(__dirname, '../FrontEnd/index.html'))
+  }
+
+app.whenReady().then(() => {
+    createWindow()
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+          app.quit()
+        }
+    })
+})
+

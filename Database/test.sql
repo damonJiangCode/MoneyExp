8956ef6209cc87aed8107e5afe077e8e603b2ebe
Damon Jiang <damonjiangwork@gmail.com>
2023-11-08 01:01:09 -0600

    DB test completed

---
+++ Database/test.sql
@@ -0,0 +1,12 @@
+-- SQLite
+-- CREATE TABLE clients (
+--     id INTEGER PRIMARY KEY,
+--     name TEXT,
+--     email TEXT
+-- );
+
+
+-- INSERT INTO clients(id, name, email) 
+-- VALUES (, "SAM", "aaa");
+
+-- SELECT * FROM clients where id=12222;
